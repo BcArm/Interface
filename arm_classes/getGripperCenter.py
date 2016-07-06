7479e3f3051481530c30f0b6ed119557458daa0b
youssefMagdy29 <youssef.magdy29@gmail.com>
2016-07-06 19:38:08 +0200

    updating getGripperCenter

--- arm_classes/getGripperCenter.py
+++ arm_classes/getGripperCenter.py
@@ -1,25 +1,7 @@
 import cv2
 import numpy as np
+
 def getGripperCenter(img):
-    def onMouse(event, x, y, f, g):
-        print("hsv" + str(hsvImg[y, x, :]))
-        print("img" + str(img[y, x, :]))
-    def getCentroids(img):
-        nLabels, labels = cv2.connectedComponents(img, 8)
-        X = [0] * nLabels
-        Y = [0] * nLabels
-        cnt = [0] * nLabels
-        for r in xrange(img.shape[0]):
-            for c in xrange(img.shape[1]):
-                label = labels[r, c]
-                X[label] += r
-                Y[label] += c
-                cnt[label] += 1
-        ret = []
-        for i in xrange(1, nLabels):
-            if (cnt[i] > 50):
-                ret.append((X[i] / cnt[i], Y[i] / cnt[i]))
-        return ret
 
     def getDistance(X, Y):
         dx = X[0] - Y[0]
@@ -44,9 +26,16 @@ def getGripperCenter(img):
     redImg = cv2.GaussianBlur(redImg, (9, 9), 0)
     yelImg = cv2.GaussianBlur(yelImg, (9, 9), 0)
 
-    cenRed = getCentroids(redImg)
-    cenYel = getCentroids(yelImg)
+    
 
+    redOut = cv2.connectedComponentsWithStats(redImg, 4, cv2.CV_32S)
+    cenRed = redOut[3] 
+    print (len(cenRed))
+
+    yelOut = cv2.connectedComponentsWithStats(yelImg, 4, cv2.CV_32S)
+    cenYel = yelOut[3] 
+    print (len(cenYel))
+    
     minD = -1
     result = (0, 0)
     for a in cenRed:
@@ -56,5 +45,6 @@ def getGripperCenter(img):
                 minD = d
                 result = ((a[0] + b[0]) / 2, (a[1] + b[1]) / 2)
 
-    return result
+    print result
+    return list(map(int, result))
 
